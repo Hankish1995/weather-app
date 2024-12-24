6ab12cd8e00c70ab17ce79e7a57552542df76674
Harmanpreetsohi <hankishbawa17@gmail.com>
2024-12-25 00:44:46 +0530

    changes saved

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,7 @@ import Layout from "./components/layout";
 import { ThemeProvider } from "./context/theme-provider";
 import WeatherDashboard from "./pages/WeatherDashboard";
 import CityPage from "./pages/CityPage";
-import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "sonner";
 
